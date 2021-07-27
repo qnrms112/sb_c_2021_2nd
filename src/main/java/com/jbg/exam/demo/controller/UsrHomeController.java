746a23503fdef806c3ac0e1718dba2288e9ecb89
qnrms112 <wlvkcjs79@naver.com>
2021-07-27 18:59:41 +0900

    게시물 컨트롤러에, 게시물 추가, 리스트 구현

--- src/main/java/com/jbg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/jbg/exam/demo/controller/UsrHomeController.java
@@ -4,27 +4,48 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.Data;
+
 @Controller
 public class UsrHomeController {
-	private int count;
-	
-	public UsrHomeController() {
-		count = -1;
-	}
 
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getString")
 	@ResponseBody
-	public int getCount() {
-		return count;
+	public String getString() {
+		return "Hi";
 	}
 	
-	@RequestMapping("/usr/home/dosetCount")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	public String dosetCount(int count) {
-		this.count = count;
-		return "count 값이 " + this.count + "값으로 초기화 되었습니다.";
+	public int getInt() {
+		return 10;
 	}
 	
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 10.5f;
+	}
 	
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+	
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+	
+	@RequestMapping("/usr/home/getCharacter")
+	@ResponseBody
+	public char getCharacter() {
+		return 'a';
+	}
 	
 }
+
+
+
---
+++ src/main/java/com/jbg/exam/deom/vo/Article.java
@@ -0,0 +1,15 @@
+package com.jbg.exam.deom.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+	public int id;
+	public String title;
+	public String body;
+}
