ad0578879f774ee63c8198ea75d0e44e8a658b68
qnrms112 <wlvkcjs79@naver.com>
2021-08-13 18:36:33 +0900

    getArticle, deleteArticle 서비스 메서드로 게시물 삭제 구현

--- src/main/java/com/jbg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jbg/exam/demo/controller/UsrArticleController.java
@@ -11,9 +11,12 @@ import com.jbg.exam.deom.vo.Article;
 
 @Controller
 public class UsrArticleController {
+	//인스턴스 변수 시작
 	private List<Article> articles;
 	private int articlesLastId;
+	//인스턴스 변수 끝
 	
+	//생성자
 	public UsrArticleController(){
 		articlesLastId = 0;
 		articles = new ArrayList<>();
@@ -21,7 +24,7 @@ public class UsrArticleController {
 		makeTestData();
 	} 
 	
-	
+	//서비스 메서드 시작
 	private void makeTestData() {
 		for(int i = 1; i <=10; i++) {
 			String title = "제목" + i;
@@ -41,10 +44,30 @@ public class UsrArticleController {
 		
 		return article;
 	}
-
+	
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	
+	//서비스 메서드 끝
+	
+	//액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title,body);
 		
 		return article;		
 	}
@@ -58,11 +81,19 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id){
-		return id + " 번 게시물을 삭제 하였습니다.";		
+		Article article = getArticle(id);
+		
+		if (article == null) {
+			return id + " 번 게시물이 존재 하지 않습니다.";		
+		}
+		
+		deleteArticle(id);
+		
+		return id + " 번 게시물을 삭제 하였습니다.";				
 	}
-	
-	
-	
+
+	//액션 메서드 끝
+		
 	
 	
 }
