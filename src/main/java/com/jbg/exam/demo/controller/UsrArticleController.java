c03bb9acc46df36de65172f70a938385c8d72fd2
qnrms112 <wlvkcjs79@naver.com>
2021-09-03 22:50:13 +0900

    db/schema.sql 파일추가

--- src/main/java/com/jbg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jbg/exam/demo/controller/UsrArticleController.java
@@ -3,71 +3,24 @@ package com.jbg.exam.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.jbg.exam.demo.service.ArticleService;
 import com.jbg.exam.deom.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	//인스턴스 변수 시작
-	private List<Article> articles;
-	private int articlesLastId;
-	//인스턴스 변수 끝
 	
-	//생성자
-	public UsrArticleController(){
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-		
-		makeTestData();
-	} 
-	
-	//서비스 메서드 시작
-	private void makeTestData() {
-		for(int i = 1; i <=10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title,body);
-		}
-	}
-	
-	
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id,title,body);
-		
-		articles.add(article);
-		articlesLastId = id;
-		
-		return article;
-	}
-	
-	private Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-	
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		
-		articles.remove(article);
-	}
-	
-	//서비스 메서드 끝
-	
+	@Autowired
+	private ArticleService articleService;
 	//액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		Article article = writeArticle(title,body);
+		Article article = articleService.writeArticle(title,body);
 		
 		return article;		
 	}
@@ -75,24 +28,48 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles(){
-		return articles;		
+		return articleService.getArticles();		
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id){
+		Article article = articleService.getArticle(id);
+		if(article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		return articleService.getArticles();		
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id){
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if (article == null) {
 			return id + " 번 게시물이 존재 하지 않습니다.";		
 		}
 		
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		
 		return id + " 번 게시물을 삭제 하였습니다.";				
 	}
 
-	//액션 메서드 끝
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body){
+		Article article = articleService.getArticle(id);
+		
+		if (article == null) {
+			return id + " 번 게시물이 존재 하지 않습니다.";		
+		}
+		
+		articleService.modifyArticle(id, title, body);
+		
+		return id + " 번 게시물을 수정 하였습니다.";				
+	}
+
 		
 	
 	
--- src/main/java/com/jbg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/jbg/exam/demo/controller/UsrHomeController.java
@@ -4,45 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.Data;
-
 @Controller
 public class UsrHomeController {
 
-	@RequestMapping("/usr/home/getString")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String getString() {
-		return "Hi";
-	}
-	
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	
-	@RequestMapping("/usr/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	
-	@RequestMapping("/usr/home/getCharacter")
-	@ResponseBody
-	public char getCharacter() {
-		return 'a';
+		return "안녕하세요.";
 	}
 	
 }
