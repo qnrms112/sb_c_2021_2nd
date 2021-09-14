7985624bb197a37c5dd41e97fbd6bc5574c4bb28
qnrms112 <wlvkcjs79@naver.com>
2021-09-14 17:51:02 +0900

    INSERT 쿼리를 메서드와 연결,last_insert_id 사용

--- src/main/java/com/jbg/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/jbg/exam/demo/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.jbg.exam.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -12,8 +13,8 @@ import com.jbg.exam.deom.vo.Article;
  
 @Mapper
 public interface ArticleRepository {
-	
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
 
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
@@ -26,6 +27,9 @@ public interface ArticleRepository {
 	
 	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 
 
 }
