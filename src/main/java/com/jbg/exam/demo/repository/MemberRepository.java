3f20970053acca0920bce3e5062bf3ac9d22d29e
qnrms112 <wlvkcjs79@naver.com>
2021-09-14 19:28:08 +0900

    회원가입을 처리하기 위해, UserMemberController, MemberService 추가

---
+++ src/main/java/com/jbg/exam/demo/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.jbg.exam.demo.repository;
+
+public class MemberRepository {
+
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		
+	}
+}
