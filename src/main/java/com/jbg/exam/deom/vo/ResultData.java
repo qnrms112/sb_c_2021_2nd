933a3def3554ebbc518579674df2d3ef524179ff
qnrms112 <wlvkcjs79@naver.com>
2021-10-01 19:08:57 +0900

    usr/article/getArticle 에 ResultData 적용usr/article/getArticle 에 ResultData 적용

---
+++ src/main/java/com/jbg/exam/deom/vo/ResultData.java
@@ -0,0 +1,39 @@
+package com.jbg.exam.deom.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	private Object data1;
+	
+	public ResultData() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static ResultData from(String resultCode, String msg) {
+		return from(resultCode, msg, null);
+	}
+	
+	
+	public static ResultData from(String resultCode, String msg, Object data1) {
+		ResultData rd = new ResultData();
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+		
+		return rd;
+	}
+	
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+	
+	public boolean isFail() {
+		return isSuccess() == false;
+	}
+	
+}
