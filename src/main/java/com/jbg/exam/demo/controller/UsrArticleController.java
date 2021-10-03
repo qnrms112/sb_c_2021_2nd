d10d554f244a9e29a74d5f927e6dd7b0f9a84a9f
qnrms112 <wlvkcjs79@naver.com>
2021-10-03 20:39:13 +0900

    게시물 리스트, JSP로 구현

--- src/main/java/com/jbg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jbg/exam/demo/controller/UsrArticleController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -54,11 +55,13 @@ public class UsrArticleController {
 		return ResultData.newData(writeArticleRd, article);
 	}
 
-	@RequestMapping("/usr/article/getArticles")
-	@ResponseBody
-	public ResultData<List<Article>> getArticles() {
+	@RequestMapping("/usr/article/list")
+	public String showList(Model model) {
 		List<Article> articles = articleService.getArticles();
-		return ResultData.from("S-1", "게시물 리스트 입니다.", articles);
+		
+		model.addAttribute("articles",articles);
+		
+		return "usr/article/list";
 	}
 
 	@RequestMapping("/usr/article/getArticle")
