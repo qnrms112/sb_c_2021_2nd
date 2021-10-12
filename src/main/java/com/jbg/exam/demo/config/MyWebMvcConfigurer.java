b3b81ffed3f9ee7e5b1d203a24b6fa0188164237
qnrms112 <wlvkcjs79@naver.com>
2021-10-12 20:13:45 +0900

    NeedLogin인터셉터 구현 및 등록NeedLogin인터셉터 구현 및 등록

--- src/main/java/com/jbg/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/jbg/exam/demo/config/MyWebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.jbg.exam.demo;
+package com.jbg.exam.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -6,8 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.jbg.exam.demo.interceptor.BeforeActionInterceptor;
-
-import lombok.Value;
+import com.jbg.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
@@ -15,10 +14,18 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
 
+	// needLoginInterceptor 인터셉터 불러오기
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
+
 	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
 				.excludePathPatterns("/error");
+
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/wirte")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
 	}
 }
--- src/main/java/com/jbg/exam/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/jbg/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -9,13 +9,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.jbg.exam.deom.vo.Rq;
 
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		Rq rq = new Rq(req);
-		req.setAttribute("rq", rq);
-
+		System.out.println("로그인 필요!!");
+		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
