465ddbe3e8e095408e2d5f4c5189d4c5e9897d68
qnrms112 <wlvkcjs79@naver.com>
2021-10-14 18:10:00 +0900

    게시물 수정폼을 보여주기전 체크게시물 수정폼을 보여주기전 체크

--- src/main/java/com/jbg/exam/deom/vo/Rq.java
+++ src/main/java/com/jbg/exam/deom/vo/Rq.java
@@ -41,17 +41,9 @@ public class Rq {
 	}
 
 	public void printHistoryBackJs(String msg) {
-        resp.setContentType("text/html; charset=UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
 		
-        println("<script>");
-		
-		if ( !Ut.empty(msg) ) {
-			println("alert('" + msg + "');");
-		}
-		
-		println("history.back();");
-		
-		println("</script>");
+		print(Ut.jsHistoryBack(msg));
 	}
 	
 	public void print(String str) {
@@ -73,6 +65,12 @@ public class Rq {
 
 	public void logout() {
 		session.removeAttribute("loginedMemberId");
+	}
+
+	public String historyBackJsOnView(String msg) {
+		req.setAttribute("msg", msg);
+		req.setAttribute("historyBack", true);
+		return "common/js";
 	} 
 
 }
