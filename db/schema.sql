bab9c49a588d208f7917df15b43f94cca21d8d83
qnrms112 <wlvkcjs79@naver.com>
2021-10-25 14:57:02 +0900

    멀티게시판 개념 도입, board 테이블 생성

--- db/schema.sql
+++ db/schema.sql
@@ -80,8 +80,8 @@ loginPw = 'user2',
 nickname = '사용자2',
 cellphoneNo = '01011111111',
 email = 'wlvkcjs79@naver.com';
- 
-#게시물 테이블에 회원정보 추가
+
+# 게시물 테이블에 회원정보 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
 
 #기존 게시물의 작성자를 2번회원으로 지정 
@@ -89,4 +89,38 @@ UPDATE article
 SET memberId = 2
 WHERE memberId = 0;
 
+#게시판 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항),free1(자유게시판1),free2(자유게시판2), ...',
+    `name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판이름',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부 (0=삭제전, 1=삭제)',
+    delDate DATETIME COMMENT '탈퇴날짜'
+);
 
+# 기본 게시판 생성
+INSERT INTO board
+SET regdate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regdate = NOW(),
+updateDate = NOW(),
+`code` = 'free1',
+`name` = '자유게시판';
+
+#게시판 테이블에 boardId 칼럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER memberId;
+
+#기존 게시물에 강제로 게시판 정보 넣기
+UPDATE article
+SET boardId =1
+WHERE id IN (1,2);
+
+UPDATE article
+SET boardId =2
+WHERE id IN (3);
