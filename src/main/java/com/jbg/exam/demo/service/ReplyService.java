171a0eca27f1c4f1fc25397996f2a3214e071267
qnrms112 <wlvkcjs79@naver.com>
2021-11-14 00:17:17 +0900

    댓글작성 처리

---
+++ src/main/java/com/jbg/exam/demo/service/ReplyService.java
@@ -0,0 +1,25 @@
+package com.jbg.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.jbg.exam.demo.repository.ReactionPointRepository;
+import com.jbg.exam.demo.repository.ReplyRepository;
+import com.jbg.exam.demo.util.Ut;
+import com.jbg.exam.demo.vo.ResultData;
+
+@Service
+public class ReplyService {
+	private ReplyRepository replyRepository;
+
+	public ReplyService(ReplyRepository replyRepository) {
+		this.replyRepository = replyRepository;
+	}
+
+	public ResultData<Integer> writeReply(int actorId, String relTypeCode, int relId, String body) {
+		replyRepository.writeReply(actorId, relTypeCode, relId, body);
+		int id = replyRepository.getLastInsertId();
+
+		return ResultData.from("S-1", Ut.f("%d번 댓글이 생성되었습니다.", id), "id", id);
+	}
+
+}
